624c19150830f17bfb64a4597420e74907414fcc
minjeong <hn01472@gmail.com>
2023-10-26 21:25:55 +0900

    JSON sever 및 페이지 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+import "./page.scss";
+
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <h2>지금 하는 일, 잘 맞을까?</h2>
-      지금 직업이 얼마나 잘 맞는지 알아보세요.
+      <p>지금 직업이 얼마나 잘 맞는지 알아보세요.</p>
+      <div className="startWrapper">
+        <Link href={`/Q1`}>
+          <p className="startText">테스트 시작하기</p>
+        </Link>
+      </div>
     </div>
   );
 }
